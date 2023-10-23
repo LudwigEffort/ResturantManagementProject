f8d93edfa1469cbe781391a7fb8b46e2f669fb5b
LudwigEffort <dr.feynman65@gmail.com>
2023-10-23 13:29:05 +0200

    test AddDIsh work

--- Tests/UnitTestDishFIleManager.cs
+++ Tests/UnitTestDishFIleManager.cs
@@ -24,4 +24,29 @@ public class UnitTestDishFileManager
         Assert.IsInstanceOf<List<Dish>>(dishesFromDb);
         Assert.IsTrue(dishesFromDb.Count > 0);
     }
+
+    [Test]
+    public void AddDish_AddsDishToDb()
+    {
+        DishFileManager dishFileManager = new DishFileManager();
+        string name = "Pasta al burro";
+        string description = "Pata con burro, parmigiano e scorza di limone";
+        double price = 9.99;
+        Dish.CategoryList category = Dish.CategoryList.FirstDish;
+        var ingredients = new List<Ingredient>
+            {
+                Ingredient.Pasta,
+                Ingredient.Formaggio
+            };
+        dishFileManager.AddDish(name, description, price, category, ingredients);
+        var dishes = dishFileManager.ReadDish();
+        bool dishAdded = dishes.Any(dish =>
+            dish.Name == name &&
+            dish.Description == description &&
+            dish.Price == price &&
+            dish.Category == category &&
+            dish.Ingredients.SequenceEqual(ingredients)
+        );
+        Assert.IsTrue(dishAdded, "The dish was not added to the db.");
+    }
 }
